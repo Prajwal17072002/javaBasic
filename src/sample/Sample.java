92f0714231023f89cc1ab0efa983eebc77818456
Prajwal17072002 <prajwalchordiya@gmail.com>
2021-09-19 18:18:40 +0530

    added class sample

---
+++ src/sample/Sample.java
@@ -0,0 +1,9 @@
+package sample;
+
+public class Sample {
+
+    public static void main(String args[]){
+
+        System.out.println("This is a sample class for commit and push into github  ");
+    }
+}
